7e19111bb10ea1a91a6dc082532a6cc70f37e02a
Tim Sutton <tim@kartoza.com>
2025-09-02 19:17:30 +0100

    Try a simpler theme

--- packages/kartoza-plymouth-theme/default.nix
+++ packages/kartoza-plymouth-theme/default.nix
@@ -3,7 +3,7 @@
 }:
 pkgs.stdenv.mkDerivation rec {
   pname = "kartoza-plymouth";
-  version = "0.0.7";
+  version = "0.0.8";
 
   src = ./src;
 
--- packages/kartoza-plymouth-theme/src/kartoza/preview.sh
+++ packages/kartoza-plymouth-theme/src/kartoza/preview.sh
@@ -12,11 +12,11 @@ B='\033[1;34m'
 G='\033[1;32m'
 
 # check if executed as root
-check_root () {
-  if [ ! $( id -u ) -eq 0 ]; then
-    echo -e $R"Must be run as root"
-    exit
-  fi
+check_root() {
+    if [ ! $(id -u) -eq 0 ]; then
+        echo -e $R"Must be run as root"
+        exit
+    fi
 }
 
 check_root
@@ -25,8 +25,14 @@ check_root
 duration=$1
 
 if [ $# -ne 1 ]; then
-	duration=10
+    duration=10
 fi
 
-plymouthd; plymouth --show-splash ; for ((I=0; I<$duration; I++)); do plymouth --update=test$I ; sleep 1; done; plymouth quit
-
+plymouthd --debug --debug-file=/tmp/plymouth-debug.log
+plymouth --show-splash
+for ((I = 0; I < $duration; I++)); do
+    plymouth --update=test$I
+    sleep 1
+done
+plymouth quit
+cat /tmp/plymouth-debug.log
